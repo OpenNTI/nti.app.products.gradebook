50f060cb218ffd4a471d82d9d57f25264aa9a4e5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-10-17 20:18:00 +0000

    add helper view
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@25704 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/__init__.py
+++ src/nti/app/products/gradebook/__init__.py
@@ -7,3 +7,6 @@ $Id$
 """
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
+
+import zope.i18nmessageid
+MessageFactory = zope.i18nmessageid.MessageFactory('nti.app.products.gradebook')
--- src/nti/app/products/gradebook/views.py
+++ src/nti/app/products/gradebook/views.py
@@ -10,13 +10,22 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from . import MessageFactory as _
+
+import simplejson
+
 from pyramid.view import view_config
 from pyramid.view import view_defaults
+from pyramid import httpexceptions as hexc
 
 from nti.appserver.dataserver_pyramid_views import _GenericGetView as GenericGetView
 
 from nti.dataserver import authorization as nauth
 
+from nti.utils.maps import CaseInsensitiveDict
+
+from . import utils
+from . import grades
 from . import interfaces as grades_interfaces
 
 _view_defaults = dict(  route_name='objects.generic.traversal',
@@ -31,6 +40,16 @@ _d_view_defaults = _view_defaults.copy()
 _d_view_defaults.update( permission=nauth.ACT_DELETE,
 						 request_method='DELETE' )
 
+class _PostView(object):
+
+	def __init__(self, request):
+		self.request = request
+
+	def readInput(self):
+		request = self.request
+		values = simplejson.loads(unicode(request.body, request.charset)) if request.body else {}
+		return CaseInsensitiveDict(**values)
+
 @view_config(context=grades_interfaces.IGradeBook)
 @view_config(context=grades_interfaces.IGradeBookPart)
 @view_config(context=grades_interfaces.IGradeBookEntry)
@@ -38,6 +57,38 @@ _d_view_defaults.update( permission=nauth.ACT_DELETE,
 class GradeBookGetView(GenericGetView):
 	""" Support for simply returning the gradebooks """
 	pass
+
+@view_config(context=grades_interfaces.IGrades)
+@view_defaults(**_c_view_defaults)
+class SetGradeView(_PostView):
+
+	def __call__(self):
+		request = self.request
+		values = self.readInput()
+		grade = grades.Grade()
+		entryId = values.get('entryId')
+		if not entryId:
+			utils.raise_field_error(request, "entryid", _("must specify a valid grade entry"))
+		grade.entryId = entryId
+	
+		value = values.get('grade')
+		if value is None:
+			utils.raise_field_error(request, "grade", _("must specify a valid grade"))
+
+		try:
+			field = grades_interfaces.IGrade['grade']
+			value = field.fromUnicode(value)
+			grade.grade = value
+		except:
+			utils.raise_field_error(request, "grade", _("must specify a valid grade"))
+
+		username = values.get('username', values.get('student'))
+		if not username:
+			utils.raise_field_error(request, "username", _("must specify a valid student name"))
+
+		store = request.context
+		store.add_grade(username, grade)
+		return hexc.HTTPNoContent()
 
 del _view_defaults
 del _c_view_defaults
