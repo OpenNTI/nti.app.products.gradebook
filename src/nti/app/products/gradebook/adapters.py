d9622bce48c019456241072c362099c7ec905de9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-04 17:39:21 +0000

    add adapter for a user/grade to a note for grade feedback
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@26567 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/adapters.py
+++ src/nti/app/products/gradebook/adapters.py
@@ -10,8 +10,14 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import six
+
 from zope import interface
 from zope import component
+
+from nti.dataserver import users
+from nti.dataserver.contenttypes import Note
+from nti.dataserver import interfaces as nti_interfaces
 
 from . import grades
 from . import interfaces as grade_interfaces
@@ -25,3 +31,22 @@ def _StringGradeAdapter(ntiid):
 @component.adapter(grade_interfaces.IGradeBookEntry)
 def _EntryGradeAdapter(entry):
 	return grades.Grade(ntiid=entry.NTIID)
+
+@interface.implementer(nti_interfaces.INote)
+@component.adapter(nti_interfaces.IUser, grade_interfaces.IGrade)
+def _DiscussionGradeAdapter(user, grade):
+	if isinstance(user, six.string_types):
+		user = users.User.get_entity(user)
+	grade = grade_interfaces.IGrade(grade)
+	container = user.getOrCreateContainer(grade.ntiid)
+	result = None
+	for obj in container.values():
+		# match the first note in container
+		if nti_interfaces.INote.providedBy(obj):
+			result = obj
+	if result is None:
+		result = Note()
+		result.creator = user
+		result.containerId = grade.ntiid
+		user.addContainedObject(result)
+	return result
