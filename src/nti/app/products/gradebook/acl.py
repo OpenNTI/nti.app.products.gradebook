60ceaf40d63e178c4132942f7e72272aab86393a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-11-06 16:25:35 +0000

    add some acl provider
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@26684 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/gradebook/acl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Relating to ACL implementations for objects defined in this package.
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from nti.dataserver import interfaces as nti_interfaces
+from nti.dataserver.authorization_acl import AbstractCreatedAndSharedACLProvider
+
+ALL_PERMISSIONS = nti_interfaces.ALL_PERMISSIONS
+
+class _GradeBookACLProvider(AbstractCreatedAndSharedACLProvider):
+	"""
+	Grant full control to their creator and inherit moderation
+	rights through their parent.
+	"""
+
+	_DENY_ALL = False
+
+	def _get_sharing_target_names( self ):
+		return ()
+
