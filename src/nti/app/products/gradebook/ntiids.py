048b1f11b6ec94cfa024e7ae723cc9ae5979cc73
Carlos Sanchez <carlos.sanchez@nextthought.com>
2013-12-06 04:45:44 +0000

    add gradebookentry resolver
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@27775 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/ntiids.py
+++ src/nti/app/products/gradebook/ntiids.py
@@ -20,26 +20,25 @@ from nti.ntiids import interfaces as nid_interfaces
 
 from . import interfaces as grades_interfaces
 
+def get_course(catalog, key):
+	for course in catalog:
+		if course.__name__ == key:
+			return course
+	return None
+
 @interface.implementer(nid_interfaces.INTIIDResolver)
 class _GradeBookResolver(object):
-
-	@classmethod
-	def get_course(self, catalog, key):
-		for course in catalog:
-			if course.__name__ == key:
-				return course
-		return None
 
 	def resolve(self, key):
 		catalog = component.queryUtility(courseware_interfaces.ICourseCatalog)
 		if catalog:
 			name = get_specific(key)
-			course = self.get_course(catalog, name)
+			course = get_course(catalog, name)
 			return grades_interfaces.IGradeBook(course, None)
 		return None
 
 @interface.implementer(nid_interfaces.INTIIDResolver)
-class _GradeBookPartResolver(_GradeBookResolver):
+class _GradeBookPartResolver(object):
 
 	def resolve(self, key):
 		catalog = component.queryUtility(courseware_interfaces.ICourseCatalog)
@@ -57,3 +56,22 @@ class _GradeBookPartResolver(_GradeBookResolver):
 				logger.error("Cannot find gradebook part using '%s'", key)
 		return None
 
+@interface.implementer(nid_interfaces.INTIIDResolver)
+class _GradeBookEntryResolver(object):
+
+	def resolve(self, key):
+		catalog = component.queryUtility(courseware_interfaces.ICourseCatalog)
+		if catalog:
+			specific = get_specific(key)
+			try:
+				course, part, entry = specific.split('.')[-3]
+				course = self.get_course(catalog, course)
+				gradebook = grades_interfaces.IGradeBook(course, None)
+				if gradebook and part in gradebook:
+					parts = gradebook[part]
+					return parts[entry]
+			except ValueError:
+				logger.error("'%s' invalid gradebook entry NTIID", key)
+			except KeyError:
+				logger.error("Cannot find gradebook entry using '%s'", key)
+		return None
