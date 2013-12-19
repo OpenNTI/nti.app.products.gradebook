05711c35d573f85e518ee2185ee65cc639fff7c3
Jason Madden <jason.madden@nextthought.com>
2013-12-19 18:39:58 +0000

    An app level test for getting the submitted assignment history as the professor.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@28380 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/tests/test_assignments.py
+++ src/nti/app/products/gradebook/tests/test_assignments.py
@@ -30,6 +30,7 @@ from nti.app.products.gradebook import assignments
 from nti.app.products.gradebook import interfaces as grades_interfaces
 
 from nti.app.testing.application_webtest import SharedApplicationTestBase
+from nti.app.testing.decorators import WithSharedApplicationMockDS
 
 import pyramid.interfaces
 
@@ -73,7 +74,7 @@ class TestAssignments(SharedApplicationTestBase):
 	@mock_dataserver.WithMockDSTrans
 	def test_get_course_assignments(self):
 
-		base = "tag:nextthought.com,2011-10:OU-HTML-CLC3403_LawAndJustice.naq.asg.assignment%s"
+		base = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.asg.assignment%s"
 		with mock_dataserver.mock_db_trans(self.ds):
 			lib = component.getUtility(IContentPackageLibrary)
 			for package in lib.contentPackages:
@@ -101,9 +102,9 @@ class TestAssignments(SharedApplicationTestBase):
 				def authenticated_userid( self, request ):
 					return 'harp4162'
 				def effective_principals(self, request):
-					return [user]
+					return ['harp4162']
 
-
+			old = component.getUtility(pyramid.interfaces.IAuthenticationPolicy)
 			request.registry.registerUtility( Policy() )
 
 			for package in lib.contentPackages:
@@ -116,3 +117,45 @@ class TestAssignments(SharedApplicationTestBase):
 					href = self.require_link_href_with_rel(ext, 'GradeSubmittedAssignmentHistory')
 					assert_that( href, is_( '/dataserver2/users/CLC3403.ou.nextthought.com/LegacyCourses/CLC3403/GradeBook/%s/Main%%20Title/SubmittedAssignmentHistory'
 											% asg.category_name))
+			request.registry.registerUtility( old )
+
+	assignment_id = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.asg.assignment1"
+	question_set_id = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.set.qset:ASMT1_ichigo"
+
+	@WithSharedApplicationMockDS(users=('harp4162'),testapp=True,default_authenticate=True)
+	def test_instructor_access_to_history_items(self):
+		# Re-enum to pick up instructor
+		with mock_dataserver.mock_db_trans(self.ds):
+			lib = component.getUtility(IContentPackageLibrary)
+			del lib.contentPackages
+			getattr(lib, 'contentPackages')
+		from nti.assessment.submission import QuestionSetSubmission
+		from nti.assessment.submission import AssignmentSubmission
+		qs_submission = QuestionSetSubmission(questionSetId=self.question_set_id)
+		submission = AssignmentSubmission(assignmentId=self.assignment_id, parts=(qs_submission,))
+
+		ext_obj = to_external_object( submission )
+		del ext_obj['Class']
+		assert_that( ext_obj, has_entry( 'MimeType', 'application/vnd.nextthought.assessment.assignmentsubmission'))
+		# Make sure we're enrolled
+		res = self.testapp.post_json( '/dataserver2/users/sjohnson@nextthought.com/Courses/EnrolledCourses',
+									  'CLC 3403',
+									  status=201 )
+
+		self.testapp.post_json( '/dataserver2/Objects/' + self.assignment_id,
+								ext_obj,
+								status=201)
+
+		instructor_environ = self._make_extra_environ(username='harp4162')
+
+		# The instructor must also be enrolled, as that's how permissioning is setup right now
+		self.testapp.post_json( '/dataserver2/users/harp4162/Courses/EnrolledCourses',
+								'CLC 3403',
+								status=201,
+								extra_environ=instructor_environ)
+		# First, it should show up in the counter
+		res = self.testapp.get('/dataserver2/Objects/' + self.assignment_id, extra_environ=instructor_environ)
+		assert_that( res.json_body, has_entry( 'GradeSubmittedCount', 1 ))
+
+		bulk_link = '/dataserver2/users/CLC3403.ou.nextthought.com/LegacyCourses/CLC3403/GradeBook/%s/Main%%20Title/SubmittedAssignmentHistory'
+		res = self.testapp.get(bulk_link, extra_environ=instructor_environ, status=404)
