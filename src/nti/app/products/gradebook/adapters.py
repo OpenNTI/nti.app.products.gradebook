b8906049aedf6c28cf754dfb6de06a12472c8016
Jason Madden <jason.madden@nextthought.com>
2014-03-19 16:41:40 +0000

    Checkpoint on the per-assignment data.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@35123 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/adapters.py
+++ src/nti/app/products/gradebook/adapters.py
@@ -15,6 +15,7 @@ from zope import component
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.app.assessment.interfaces import IUsersCourseAssignmentHistoryItem
+from nti.app.assessment.interfaces import IUsersCourseAssignmentHistory
 
 from .interfaces import IGrade
 from .interfaces import IGradeBook
@@ -57,6 +58,7 @@ def _no_pickle(*args):
 
 @interface.implementer(IGrade)
 def grade_for_history_item(item):
+	"Registered as an adapter for both history item and summary"
 	course = ICourseInstance(item)
 	user = IUser(item) # Can we do this with just the item? item.creator?
 	book = IGradeBook(course)
@@ -78,3 +80,25 @@ def grade_for_history_item(item):
 
 		return grade
 	return None
+
+@interface.implementer(IUsersCourseAssignmentHistoryItem)
+@component.adapter(IGrade)
+def history_item_for_grade(grade):
+	course = ICourseInstance(grade)
+	user = IUser(grade)
+
+	history = component.getMultiAdapter( (course, user),
+										 IUsersCourseAssignmentHistory)
+
+	assg_id = grade.__parent__.AssignmentId
+	try:
+		return history[assg_id]
+	except KeyError:
+		raise TypeError("No history for grade")
+
+from nti.dataserver.users import User
+
+@interface.implementer(IUser)
+@component.adapter(IGrade)
+def grade_to_user(grade):
+	return User.get_user(grade.__name__)
