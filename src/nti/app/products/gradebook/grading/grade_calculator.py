59937bfa05b8ba3ec861be3763f05342a60ee021
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-09 03:19:23 +0000

    check point.. script to calculate the current grade of a course
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@56927 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/gradebook/grading/grade_calculator.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+from zope.security.interfaces import IPrincipal
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from nti.monkey import relstorage_patch_all_except_gevent_on_import
+relstorage_patch_all_except_gevent_on_import.patch()
+
+import os
+import argparse
+import importlib
+
+from zope.component import hooks
+
+from zope.configuration import xmlconfig, config
+from zope.dottedname import resolve as dottedname
+
+from zope.container.contained import Contained
+from zope.container.interfaces import INameChooser
+
+import zope.browserpage
+
+from z3c.autoinclude.zcml import includePluginsDirective
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseEnrollments
+
+from nti.dataserver.utils import run_with_dataserver
+
+from nti.ntiids.ntiids import find_object_with_ntiid
+
+from nti.site.site import get_site_for_site_names
+
+from ..interfaces import IGradeScheme
+from ..interfaces import NO_SUBMIT_PART_NAME
+from ..assignments import create_assignment_part
+
+from . import find_grading_policy_for_course
+
+class PluginPoint(Contained):
+
+	def __init__(self, name):
+		self.__name__ = name
+
+PP_APP = PluginPoint('nti.app')
+PP_APP_SITES = PluginPoint('nti.app.sites')
+PP_APP_PRODUCTS = PluginPoint('nti.app.products')
+
+def create_context(env_dir=None, with_library=True):
+	etc = os.getenv('DATASERVER_ETC_DIR') or os.path.join(env_dir, 'etc')
+	etc = os.path.expanduser(etc)
+
+	context = config.ConfigurationMachine()
+	xmlconfig.registerCommonDirectives(context)
+
+	slugs = os.path.join(etc, 'package-includes')
+	if os.path.exists(slugs) and os.path.isdir(slugs):
+		package = dottedname.resolve('nti.dataserver')
+		context = xmlconfig.file('configure.zcml', package=package, context=context)
+		xmlconfig.include(context, files=os.path.join(slugs, '*.zcml'),
+						  package='nti.appserver')
+
+	if with_library:
+		library_zcml = os.path.join(etc, 'library.zcml')
+		if not os.path.exists(library_zcml):
+			raise Exception("Could not locate library zcml file %s", library_zcml)
+		xmlconfig.include(context, file=library_zcml, package='nti.appserver')
+		
+	# Include zope.browserpage.meta.zcm for tales:expressiontype
+	# before including the products
+	xmlconfig.include(context, file="meta.zcml", package=zope.browserpage)
+
+	# include plugins
+	includePluginsDirective(context, PP_APP)
+	includePluginsDirective(context, PP_APP_SITES)
+	includePluginsDirective(context, PP_APP_PRODUCTS)
+	
+	return context
+
+def calculate_grades(ntiid, scheme, entry_name='Current_Grade', site=None):
+
+	module_name, class_name = scheme.rsplit(".", 1)
+	module = importlib.import_module(module_name)
+	clazz = getattr(module, class_name)
+	grade_scheme = clazz()
+	if IGradeScheme.providedBy(grade_scheme):
+		raise ValueError("Invalid grade scheme class")
+
+	if site:
+		cur_site = hooks.getSite()
+		new_site = get_site_for_site_names( (site,), site=cur_site )
+		if new_site is cur_site:
+			raise ValueError("Unknown site name", site)
+		hooks.setSite(new_site)
+	
+	context = find_object_with_ntiid(ntiid)
+	course = ICourseInstance(context, None)
+	if course is None:
+		raise ValueError("Unknown course", ntiid)
+
+	policy = find_grading_policy_for_course(course)
+	if policy is None:
+		raise ValueError("Course does not have grading policy")
+	
+	part = create_assignment_part(course, NO_SUBMIT_PART_NAME)
+	entry = part.getEntryByAssignment(entry_name)
+	if entry is None:
+		order = len(part) + 1
+		entry = part.entryFactory(displayName=entry_name, order=order, AssignmentId=entry_name)
+		part[INameChooser(part).chooseName(entry_name, entry)] = entry
+		
+	for record in ICourseEnrollments(course).iter_enrollments():
+		principal = record.principal
+		grade = policy.grade(principal)
+		grade = grade_scheme.fromCorrectness(grade)
+		entry[IPrincipal(principal).id] = grade
+
+def main():
+	arg_parser = argparse.ArgumentParser(description="Grade calculator")
+	arg_parser.add_argument('-v', '--verbose', help="Be verbose", action='store_true',
+							 dest='verbose')
+	
+	arg_parser.add_argument('course', help="Course NTIID")
+	
+	arg_parser.add_argument('-s', '--site', dest='site', help="Request site")
+	
+	arg_parser.add_argument('-g', '--grade', dest='scheme', help="Grade scheme class name",
+							default='nti.app.products.gradebook.gradescheme.LetterGradeScheme')
+	
+	arg_parser.add_argument('-e', '--entry', dest='entry', help="Grade entry name",
+							default='Current_Grade')
+	
+	args = arg_parser.parse_args()
+	verbose = args.verbose
+	
+	site = args.site
+	if not site and verbose:
+		print('WARN: NO site specified')
+
+	env_dir = os.getenv('DATASERVER_DIR')
+	context = create_context(env_dir)
+	conf_packages = ('nti.appserver',)
+	run_with_dataserver(environment_dir=env_dir,
+						verbose=verbose,
+						context=context,
+						minimal_ds=True,
+						xmlconfig_packages=conf_packages,
+						function=lambda: calculate_grades(site=site,
+														  entry=args.entry,
+														  ntiid=args.course,
+														  scheme=args.scheme))
+
+if __name__ == '__main__':
+	main()
+	
