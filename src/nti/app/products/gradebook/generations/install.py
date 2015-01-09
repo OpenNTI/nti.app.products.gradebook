b188e7f69eb5372c76f1cf78de3392661c1c471a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-09 03:54:01 +0000

    add placeholder for future migrations
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@56936 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/products/gradebook/generations/install.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Generations for managing gradebook.
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 1
+
+from zope.generations.generations import SchemaManager
+
+class _GradeBookSchemaManager(SchemaManager):
+	"""
+	A schema manager that we can register as a utility in ZCML.
+	"""
+	
+	def __init__( self ):
+		super(_GradeBookSchemaManager, self).__init__(generation=generation,
+													  minimum_generation=generation,
+													  package_name='nti.app.products.gradebook')
+
+def evolve( context ):
+	return context
---
+++ src/nti/app/products/gradebook/generations/tests/test_install.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import has_key
+from hamcrest import assert_that
+
+from nti.dataserver.tests import mock_dataserver
+from nti.dataserver.tests.mock_dataserver import WithMockDSTrans
+from nti.dataserver.tests.mock_dataserver import DataserverLayerTest
+
+class TestFunctionalInstall(DataserverLayerTest):
+
+	@WithMockDSTrans
+	def test_installed(self):
+		conn = mock_dataserver.current_transaction
+		root = conn.root()
+		generations = root['zope.generations']
+		assert_that( generations, has_key('nti.dataserver-products-gradebooks'))
