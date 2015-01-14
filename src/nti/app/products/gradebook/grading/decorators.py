845b57ead9566e95dc2088cf862c5663437e1558
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-14 17:26:04 +0000

    back to course b/c students don't have access to the gradebook yet
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@57620 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/grading/decorators.py
+++ src/nti/app/products/gradebook/grading/decorators.py
@@ -9,30 +9,27 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from zope import component
 from zope import interface
 
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
+
+from nti.app.products.courseware.utils import is_enrolled
 
 from nti.dataserver.links import Link
 
 from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import IExternalMappingDecorator
 
-from ..interfaces import IGradeBook
-from ..decorators import gradebook_readable
-
 from . import find_grading_policy_for_course
 
 LINKS = StandardExternalFields.LINKS
 
-@component.adapter(IGradeBook)
 @interface.implementer(IExternalMappingDecorator)
 class _CurrentGradeLinkDecorator(AbstractAuthenticatedRequestAwareDecorator):
 
 	def _predicate(self, context, result):
-		result = self._is_authenticated and gradebook_readable(context) and \
-				 find_grading_policy_for_course(context) is not None
+		result = self._is_authenticated and find_grading_policy_for_course(context) and \
+				 is_enrolled(context, self.remoteUser)
 		return result
 	
 	def _do_decorate_external(self, context, result):
