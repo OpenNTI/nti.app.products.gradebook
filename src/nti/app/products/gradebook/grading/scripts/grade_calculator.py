4a58cd32871cf447207307085aac5b00d6b525dd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-16 16:22:11 +0000

    use common script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@59512 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/grading/scripts/grade_calculator.py
+++ src/nti/app/products/gradebook/grading/scripts/grade_calculator.py
@@ -9,97 +9,39 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from nti.monkey import relstorage_patch_all_except_gevent_on_import
-relstorage_patch_all_except_gevent_on_import.patch()
-
 import os
 import argparse
 import importlib
 
-from zope.component import hooks
-
-from zope.configuration import xmlconfig, config
-from zope.dottedname import resolve as dottedname
-
-from zope.container.contained import Contained
-
-import zope.browserpage
-
-from z3c.autoinclude.zcml import includePluginsDirective
-
 from nti.contenttypes.courses.interfaces import ICourseInstance
 
 from nti.dataserver.utils import run_with_dataserver
+from nti.dataserver.utils.base_script import set_site
+from nti.dataserver.utils.base_script import create_context
 
 from nti.ntiids.ntiids import find_object_with_ntiid
-
-from nti.site.site import get_site_for_site_names
 
 from ..interfaces import IGradeScheme
 
 from .. import calculate_grades
 
-class PluginPoint(Contained):
-
-	def __init__(self, name):
-		self.__name__ = name
-
-PP_APP = PluginPoint('nti.app')
-PP_APP_SITES = PluginPoint('nti.app.sites')
-PP_APP_PRODUCTS = PluginPoint('nti.app.products')
-
-def create_context(env_dir=None, with_library=True):
-	etc = os.getenv('DATASERVER_ETC_DIR') or os.path.join(env_dir, 'etc')
-	etc = os.path.expanduser(etc)
-
-	context = config.ConfigurationMachine()
-	xmlconfig.registerCommonDirectives(context)
-
-	slugs = os.path.join(etc, 'package-includes')
-	if os.path.exists(slugs) and os.path.isdir(slugs):
-		package = dottedname.resolve('nti.dataserver')
-		context = xmlconfig.file('configure.zcml', package=package, context=context)
-		xmlconfig.include(context, files=os.path.join(slugs, '*.zcml'),
-						  package='nti.appserver')
-
-	if with_library:
-		library_zcml = os.path.join(etc, 'library.zcml')
-		if not os.path.exists(library_zcml):
-			raise Exception("Could not locate library zcml file %s", library_zcml)
-		xmlconfig.include(context, file=library_zcml, package='nti.appserver')
-		
-	# Include zope.browserpage.meta.zcm for tales:expressiontype
-	# before including the products
-	xmlconfig.include(context, file="meta.zcml", package=zope.browserpage)
-
-	# include plugins
-	includePluginsDirective(context, PP_APP)
-	includePluginsDirective(context, PP_APP_SITES)
-	includePluginsDirective(context, PP_APP_PRODUCTS)
-	
-	return context
-
-def _process_args(ntiid, scheme, site=None, entry_name=None, verbose=False):
+def _process_args(ntiid, scheme, usernames=(), site=None, entry_name=None, verbose=False):
 	module_name, class_name = scheme.rsplit(".", 1)
 	module = importlib.import_module(module_name)
-	clazz = getattr(module, class_name)
-	grade_scheme = clazz()
+	grade_scheme = getattr(module, class_name)()
 	if not IGradeScheme.providedBy(grade_scheme):
 		raise ValueError("Invalid grade scheme class")
 
 	if site:
-		cur_site = hooks.getSite()
-		new_site = get_site_for_site_names( (site,), site=cur_site )
-		if new_site is cur_site:
-			raise ValueError("Unknown site name", site)
-		hooks.setSite(new_site)
+		set_site(site)
 	
 	context = find_object_with_ntiid(ntiid)
 	course = ICourseInstance(context, None)
 	if course is None:
-		raise ValueError("Unknown course", ntiid)
+		raise ValueError("Course not found", ntiid)
 
-	result = calculate_grades(course, grade_scheme, entry_name=entry_name)
+	result = calculate_grades(course, usernames=usernames, grade_scheme=grade_scheme, 
+							  entry_name=entry_name)
 	if not entry_name or verbose:
 		print("\nGrades...")
 		for name, grade in result.items():
@@ -115,6 +57,11 @@ def main():
 	arg_parser.add_argument('-g', '--grade', dest='scheme', help="Grade scheme class name",
 							default='nti.app.products.gradebook.gradescheme.LetterGradeScheme')
 	arg_parser.add_argument('-e', '--entry', dest='entry', help="Grade entry name")
+	arg_parser.add_argument('-u', '--users',
+							 dest='usernames',
+							 nargs="+",
+							 default=(),
+							 help="The usernames")
 	args = arg_parser.parse_args()
 	verbose = args.verbose
 	
@@ -123,7 +70,7 @@ def main():
 		print('WARN: NO site specified')
 
 	env_dir = os.getenv('DATASERVER_DIR')
-	context = create_context(env_dir)
+	context = create_context(env_dir, with_library=True)
 	conf_packages = ('nti.appserver',)
 	run_with_dataserver(environment_dir=env_dir,
 						verbose=verbose,
@@ -134,7 +81,8 @@ def main():
 														verbose=verbose,
 														ntiid=args.ntiid,
 														scheme=args.scheme,
-														entry_name=args.entry))
+														entry_name=args.entry,
+														usernames=args.usernames))
 
 if __name__ == '__main__':
 	main()
