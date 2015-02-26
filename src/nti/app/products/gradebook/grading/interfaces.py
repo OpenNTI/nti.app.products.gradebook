a31c3f4f5be58e8dea18f7d284bc61a39545f0d4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-02-26 03:47:39 +0000

    remove field
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@60290 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/grading/interfaces.py
+++ src/nti/app/products/gradebook/grading/interfaces.py
@@ -19,7 +19,6 @@ from nti.contenttypes.courses.grading.interfaces import ICategoryGradeScheme as 
 
 from nti.schema.field import Int
 from nti.schema.field import Dict
-from nti.schema.field import Number
 from nti.schema.field import Object
 from nti.schema.field import ValidTextLine
 
@@ -32,7 +31,6 @@ class IAssigmentGradeScheme(interface.Interface):
 class ICategoryGradeScheme(ICTGCategoryGradeScheme):
 	GradeScheme = Object(IGradeScheme, required=False)
 	DropLowest = Int(title="Drop lowest grade in category", min=0, required=False)
-	LatePenalty = Number(title="Late penalty", default=1, min=0.0, max=1.0, required=True)
 
 class ICS1323EqualGroupGrader(IEqualGroupGrader):	
 	Groups = Dict(key_type=ValidTextLine(title="Category Name"),
