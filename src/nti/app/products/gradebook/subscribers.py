f58920520505d4d9216b0bd4bec98771eb2844ca
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-02 16:24:45 +0000

    use new located funcs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@82177 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/subscribers.py
+++ src/nti/app/products/gradebook/subscribers.py
@@ -34,23 +34,17 @@ from pyramid.traversal import find_interface
 from nti.app.assessment.interfaces import IUsersCourseAssignmentHistory
 from nti.app.assessment.interfaces import IUsersCourseAssignmentHistoryItem
 
-from nti.app.products.gradebook.assignments import synchronize_gradebook
-
-from nti.app.products.gradebook.autograde_policies import find_autograde_policy_for_assignment_in_course
-
-from nti.app.products.gradebook.grades import PersistentGrade
-
-from nti.app.products.gradebook.grading import IGradeBookGradingPolicy
-from nti.app.products.gradebook.grading import find_grading_policy_for_course
-
 from nti.app.products.gradebook.index import IX_STUDENT
 from nti.app.products.gradebook.index import CATALOG_NAME
 
 from nti.app.products.gradebook.interfaces import IGrade
 from nti.app.products.gradebook.interfaces import IGradeBook
 
-from nti.app.products.gradebook.utils import save_in_container
 from nti.app.products.gradebook.utils import remove_from_container
+
+from nti.app.products.gradebook.utils.gradebook import find_entry_for_item
+from nti.app.products.gradebook.utils.gradebook import set_grade_by_assignment_history_item
+from nti.app.products.gradebook.utils.gradebook import synchronize_gradebook_and_verify_policy
 
 from nti.containers.containers import CaseInsensitiveLastModifiedBTreeContainer
 
@@ -62,12 +56,12 @@ from nti.contenttypes.courses.interfaces import RID_INSTRUCTOR
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseInstanceAvailableEvent
 
+from nti.dataserver.activitystream_change import Change
+
 from nti.dataserver.interfaces import IUser
 
 from nti.dataserver.users import User
 from nti.dataserver.users.interfaces import IWillDeleteEntityEvent
-
-from nti.dataserver.activitystream_change import Change
 
 def find_gradebook_in_lineage(obj):
 	book = find_interface(obj, IGradeBook)
@@ -102,56 +96,17 @@ def _grade_modified(grade, event):
 		item = assignment_history[entry.assignmentId]
 		item.updateLastModIfGreater(grade.lastModified)
 
-def _find_entry_for_item(item):
-	assignmentId = item.Submission.assignmentId
-	course = ICourseInstance(item, None)
-	if course is None:
-		# Typically during tests
-		logger.warning("Assignment %s has no course", assignmentId)
-		return None
-
-	book = IGradeBook(course)
-
-	entry = book.getColumnForAssignmentId(assignmentId)
-	if entry is None:
-		# Typically during tests something is added
-		_synchronize_gradebook_with_course_instance(course, None)
-		entry = book.getColumnForAssignmentId(assignmentId)
-	if entry is None:
-		# Also typically during tests.
-		# TODO: Fix those tests to properly register assignments
-		# so this branch goes away
-		logger.warning("Assignment %s not found in course %s", assignmentId, course)
-		return
-
-	return entry
-
 @component.adapter(IUsersCourseAssignmentHistoryItem, IObjectAddedEvent)
 def _assignment_history_item_added(item, event):
-	entry = _find_entry_for_item(item)
-	if entry is not None:
-		user = IUser(item)
-		grade = PersistentGrade()
-		grade.username = user.username
+	set_grade_by_assignment_history_item(item)
 
-		# If there is an auto-grading policy for the course instance,
-		# then let it convert the auto-assessed part of the submission
-		# into the initial grade value
-		course = ICourseInstance(item)
-		assignmentId = item.Submission.assignmentId
-		policy = find_autograde_policy_for_assignment_in_course(course, assignmentId)
-		if policy is not None:
-			autograde = policy.autograde(item.pendingAssessment)
-			if autograde is not None:
-				grade.AutoGrade, grade.AutoGradeMax = autograde
-			if grade.value is None:
-				grade.value = grade.AutoGrade
-		# Finally after we finish filling it in, publish it
-		save_in_container(entry, user.username, grade)
+@component.adapter(IUsersCourseAssignmentHistoryItem, IObjectModifiedEvent)
+def _assignment_history_item_modifiedl(item, event):
+	set_grade_by_assignment_history_item(item)
 
 @component.adapter(IUsersCourseAssignmentHistoryItem, IObjectRemovedEvent)
 def _assignment_history_item_removed(item, event):
-	entry = _find_entry_for_item(item)
+	entry = find_entry_for_item(item)
 	if entry is not None:
 		user = IUser(item, None)
 		if user is not None:
@@ -163,12 +118,7 @@ def _assignment_history_item_removed(item, event):
 
 @component.adapter(ICourseInstance, ICourseInstanceAvailableEvent)
 def _synchronize_gradebook_with_course_instance(course, event):
-	synchronize_gradebook(course)
-	# CS: We verify the grading policy after
-	# the gradebook has been synchronized
-	policy = find_grading_policy_for_course(course)
-	if policy is not None and IGradeBookGradingPolicy.providedBy(policy):
-		policy.verify()
+	synchronize_gradebook_and_verify_policy(course)
 
 # Storing notable items.
 # We keep a parallel datastructure holding persistent objects
