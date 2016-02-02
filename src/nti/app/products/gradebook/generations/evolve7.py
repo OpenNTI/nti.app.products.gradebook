3b6020aa2cb55097a7b4bc1d22f1707832e719e3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-02 21:33:42 +0000

    load library first
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@82223 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/generations/evolve7.py
+++ src/nti/app/products/gradebook/generations/evolve7.py
@@ -12,6 +12,7 @@ logger = __import__('logging').getLogger(__name__)
 generation = 7
 
 from zope import component
+from zope import interface
 
 from zope.component.hooks import site
 from zope.component.hooks import setHooks
@@ -26,9 +27,26 @@ from nti.app.products.gradebook.index import install_grade_catalog
 
 from nti.app.products.gradebook.interfaces import IGrade
 
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
 from nti.dataserver.interfaces import IMetadataCatalog
 
 from nti.dataserver.metadata_index import CATALOG_NAME
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+	root = None
+
+	def get_by_oid(self, oid, ignore_creator=False):
+		resolver = component.queryUtility(IOIDResolver)
+		if resolver is None:
+			logger.warn("Using dataserver without a proper ISiteManager configuration.")
+		else:
+			return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+		return None
 
 def do_evolve(context, generation=generation):
 	logger.info("Gradebook evolution %s started", generation);
@@ -39,9 +57,18 @@ def do_evolve(context, generation=generation):
 	lsm = ds_folder.getSiteManager()
 	intids = lsm.getUtility(IIntIds)
 
+	mock_ds = MockDataserver()
+	mock_ds.root = ds_folder
+	component.provideUtility(mock_ds, IDataserver)
+
 	with site(ds_folder):
 		assert 	component.getSiteManager() == ds_folder.getSiteManager(), \
 				"Hooks not installed?"
+
+		# load library
+		library = component.queryUtility(IContentPackageLibrary)
+		if library is not None:
+			library.syncContentPackages()
 
 		catalog = install_grade_catalog(ds_folder, intids)
 		if IX_SITE not in catalog:
@@ -49,7 +76,7 @@ def do_evolve(context, generation=generation):
 			intids.register(index)
 			locate(index, catalog, IX_SITE)
 			catalog[IX_SITE] = index
-	
+
 			metadata = lsm.getUtility(IMetadataCatalog, name=CATALOG_NAME)
 			query = {
 				'mimeType': {'any_of': ('application/vnd.nextthought.grade',)}
