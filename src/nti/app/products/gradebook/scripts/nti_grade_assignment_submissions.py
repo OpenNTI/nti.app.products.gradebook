525ca6fda9bf0216dec8442ed2c3aec85ff97e50
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-11 22:41:15 +0000

    use info
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@82777 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/scripts/nti_grade_assignment_submissions.py
+++ src/nti/app/products/gradebook/scripts/nti_grade_assignment_submissions.py
@@ -65,7 +65,7 @@ def _process_args(args):
 			continue
 		grade = set_grade_by_assignment_history_item(item, args.overwrite)
 		if grade is None:
-			logger.warn("Could not set grade for submission for user %s. Empty Gradebook?", 
+			logger.info("Could not set grade for submission for user %s. Empty Gradebook?", 
 						username)
 		elif args.verbose:
 			logger.info("Setting grade for user %s to %s", username, grade.value)
