e4818316ee9e0c5e82468d8f451fbb64cc112944
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-07-17 22:42:14 +0000

    use index to resolve NTIIDs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@92453 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/ntiids.py
+++ src/nti/app/products/gradebook/ntiids.py
@@ -12,66 +12,77 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
+from zope.intid.interfaces import IIntIds
+
 from nti.app.products.gradebook.interfaces import IGradeBook
 
-from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses import get_courses_catalog
+
+from nti.contenttypes.courses.index import IX_NAME
+from nti.contenttypes.courses.index import IX_SITE
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
 from nti.ntiids.interfaces import INTIIDResolver
 
 from nti.ntiids.ntiids import get_specific
 
-def get_course(catalog, key):
-	for course in catalog.iterCatalogEntries():
-		if course.__name__ == key:
-			return course
+from nti.site.site import get_component_hierarchy_names
+
+def get_course(key):
+	sites = get_component_hierarchy_names()
+	query = {
+		IX_NAME: {'any_of': key},
+		IX_SITE: {'any_of': sites}
+	}
+	catalog = get_courses_catalog()
+	intids = component.getUtility(IIntIds)
+	for doc_id in catalog.apply(query) or ():
+		course = intids.queryObject(doc_id)
+		if ICourseInstance.providedBy(course):
+			return ICourseCatalogEntry(course, None)
 	return None
 
 @interface.implementer(INTIIDResolver)
 class _GradeBookResolver(object):
 
 	def resolve(self, key):
-		catalog = component.queryUtility(ICourseCatalog)
-		if catalog is not None:
-			name = get_specific(key)
-			course = get_course(catalog, name)
-			return IGradeBook(course, None)
-		return None
+		name = get_specific(key)
+		course = get_course(name)
+		return IGradeBook(course, None)
 
 @interface.implementer(INTIIDResolver)
 class _GradeBookPartResolver(object):
 
 	def resolve(self, key):
-		catalog = component.queryUtility(ICourseCatalog)
-		if catalog is not None:
-			specific = get_specific(key)
-			try:
-				course, part = specific.split('.')[-2]
-				course = get_course(catalog, course)
-				gradebook = IGradeBook(course, None)
-				if gradebook:
-					return gradebook[part]
-			except ValueError:
-				logger.error("'%s' invalid gradebook part NTIID", key)
-			except KeyError:
-				logger.error("Cannot find gradebook part using '%s'", key)
+		specific = get_specific(key)
+		try:
+			course, part = specific.split('.')[-2]
+			course = get_course(course)
+			gradebook = IGradeBook(course, None)
+			if gradebook:
+				return gradebook[part]
+		except ValueError:
+			logger.error("'%s' invalid gradebook part NTIID", key)
+		except KeyError:
+			logger.error("Cannot find gradebook part using '%s'", key)
 		return None
 
 @interface.implementer(INTIIDResolver)
 class _GradeBookEntryResolver(object):
 
 	def resolve(self, key):
-		catalog = component.queryUtility(ICourseCatalog)
-		if catalog is not None:
-			specific = get_specific(key)
-			try:
-				course, part, entry = specific.split('.')[-3]
-				course = get_course(catalog, course)
-				gradebook = IGradeBook(course, None)
-				if gradebook and part in gradebook:
-					parts = gradebook[part]
-					return parts[entry]
-			except ValueError:
-				logger.error("'%s' invalid gradebook entry NTIID", key)
-			except KeyError:
-				logger.error("Cannot find gradebook entry using '%s'", key)
+		specific = get_specific(key)
+		try:
+			course, part, entry = specific.split('.')[-3]
+			course = get_course(course)
+			gradebook = IGradeBook(course, None)
+			if gradebook and part in gradebook:
+				parts = gradebook[part]
+				return parts[entry]
+		except ValueError:
+			logger.error("'%s' invalid gradebook entry NTIID", key)
+		except KeyError:
+			logger.error("Cannot find gradebook entry using '%s'", key)
 		return None
