106b8497a445c11ecd926b597395097d27adda89
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-20 20:20:20 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@104918 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/assignments.py
+++ src/nti/app/products/gradebook/assignments.py
@@ -29,101 +29,107 @@ from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 _assignment_comparator = assignment_comparator
 get_course_assignments = get_course_assignments
 
+
 def create_assignment_part(course, part_name, _book=None):
-	book = _book if _book is not None else IGradeBook(course)
-	if part_name not in book:
-		if part_name == NO_SUBMIT_PART_NAME:
-			factory = NoSubmitGradeBookPart
-		else:
-			factory = GradeBookPart
+    book = _book if _book is not None else IGradeBook(course)
+    if part_name not in book:
+        if part_name == NO_SUBMIT_PART_NAME:
+            factory = NoSubmitGradeBookPart
+        else:
+            factory = GradeBookPart
 
-		part = factory(displayName=part_name,
-					   order=1)  # Order makes very little sense here...
+        part = factory(displayName=part_name,
+                       order=1)  # Order makes very little sense here...
 
-		book[INameChooser(book).chooseName(part_name, part)] = part
-	return book[part_name]
+        book[INameChooser(book).chooseName(part_name, part)] = part
+    return book[part_name]
 get_or_create_assignment_part = create_assignment_part
 
+
 def create_assignment_entry(course, assignment, displayName, order=1, _book=None):
-	book = _book if _book is not None else IGradeBook(course)
+    book = _book if _book is not None else IGradeBook(course)
 
-	assignmentId = assignment.__name__
+    assignmentId = assignment.__name__
 
-	entry = book.getColumnForAssignmentId(assignmentId)
-	if entry is None:
-		part = get_or_create_assignment_part(course, assignment.category_name)
-		# Hmm, maybe we should just ask it to create the entry
-		part.validateAssignment(assignment)
-		entry = part.entryFactory(displayName=displayName,
-							   	  order=order,
-							   	  AssignmentId=assignmentId)
-		part[INameChooser(part).chooseName(displayName, entry)] = entry
-	elif entry.displayName != displayName:
-		entry.displayName = displayName
+    entry = book.getColumnForAssignmentId(assignmentId)
+    if entry is None:
+        part = get_or_create_assignment_part(course, assignment.category_name)
+        # Hmm, maybe we should just ask it to create the entry
+        part.validateAssignment(assignment)
+        entry = part.entryFactory(displayName=displayName,
+                                  order=order,
+                                  AssignmentId=assignmentId)
+        part[INameChooser(part).chooseName(displayName, entry)] = entry
+    elif entry.displayName != displayName:
+        entry.displayName = displayName
 
-	return entry
+    return entry
 get_or_create_assignment_entry = create_assignment_entry
 
+
 def synchronize_gradebook(context):
-	"""
-	Makes the gradebook for the course match the assignments for the course.
+    """
+    Makes the gradebook for the course match the assignments for the course.
 
-	New assignments are added to a part matching their category. If an assignment
-	exists in the book but not the course assignments, and there are
-	no recorded grades, it is removed.
-	"""
-	course = ICourseInstance(context, None)
-	if course is None:
-		return
-	catalog_entry = ICourseCatalogEntry( course, None )
-	course_ntiid = getattr( catalog_entry, 'ntiid', '' )
-	logger.info( "Syncing gradebook for %s", course_ntiid )
+    New assignments are added to a part matching their category. If an assignment
+    exists in the book but not the course assignments, and there are
+    no recorded grades, it is removed.
+    """
+    course = ICourseInstance(context, None)
+    if course is None:
+        return
+    catalog_entry = ICourseCatalogEntry(course, None)
+    course_ntiid = getattr(catalog_entry, 'ntiid', '')
+    logger.info("Syncing gradebook for %s", course_ntiid)
 
-	assignment_ids = set()
-	book = IGradeBook(course)
-	assignments = get_all_course_assignments(course)
+    assignment_ids = set()
+    book = IGradeBook(course)
+    assignments = get_all_course_assignments(course)
 
-	category_map = {}
-	for part in book.values():
-		for entry in part.values():
-			category_map[entry.assignmentId] = part.__name__
+    category_map = {}
+    for part in book.values():
+        for entry in part.values():
+            category_map[entry.assignmentId] = part.__name__
 
-	# FIXME: What if an assignment changes parts (category_name)?
-	# We'll need to move them
-	for idx, assignment in enumerate(assignments):
-		ordinal = idx + 1
-		assignment_ids.add(assignment.__name__)
-		if assignment.title:
-			displayName = assignment.title
-		else:
-			displayName = 'Assignment %s' % ordinal
-		create_assignment_entry(course, assignment, displayName, ordinal, book)
+    # FIXME: What if an assignment changes parts (category_name)?
+    # We'll need to move them
+    for idx, assignment in enumerate(assignments):
+        ordinal = idx + 1
+        assignment_ids.add(assignment.__name__)
+        if assignment.title:
+            displayName = assignment.title
+        else:
+            displayName = 'Assignment %s' % ordinal
+        create_assignment_entry(course, assignment, displayName, ordinal, book)
 
-	# Now drop entries that don't correspond to existing assignments
-	# and that don't have grades
-	for part in book.values():
-		for entry in part.values():
-			if entry.assignmentId not in assignment_ids and len(entry) == 0:
-				try:
-					del part[entry.__name__]
-					logger.info( 'Removed %s from gradebook for %s',
-								 entry.assignmentId,
-								 course_ntiid )
-				except TypeError:
-					logger.warning("Failed to remove part %s", part, exc_info=True)
+    # Now drop entries that don't correspond to existing assignments
+    # and that don't have grades
+    for part in book.values():
+        for entry in part.values():
+            if entry.assignmentId not in assignment_ids and len(entry) == 0:
+                try:
+                    del part[entry.__name__]
+                    logger.info('Removed %s from gradebook for %s',
+                                entry.assignmentId,
+                                course_ntiid)
+                except TypeError:
+                    logger.warning("Failed to remove part %s",
+								   part, exc_info=True)
 
-		if not part:
-			del book[part.__name__]
+        if not part:
+            del book[part.__name__]
 
-	# Now check consistency due to the above fixme. Until we get it fixed,
-	# don't let it happen
-	entry_aids = set()
-	for part in book.values():
-		for entry in part.values():
-			if entry.AssignmentId in entry_aids:
-				raise ValueError("An assignment changed categories. Not currently allowed.")
-				# use svn history to figure out what it used to be and change it back
-			entry_aids.add(entry.AssignmentId)
+    # Now check consistency due to the above fixme. Until we get it fixed,
+    # don't let it happen
+    entry_aids = set()
+    for part in book.values():
+        for entry in part.values():
+            if entry.AssignmentId in entry_aids:
+                raise ValueError(
+                    "An assignment changed categories. Not currently allowed.")
+                # use svn history to figure out what it used to be and change
+                # it back
+            entry_aids.add(entry.AssignmentId)
 
-	result = len(assignments)
-	return result
+    result = len(assignments)
+    return result
