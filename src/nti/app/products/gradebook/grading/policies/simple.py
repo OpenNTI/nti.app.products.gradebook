9d70b8f6082d0f0c4cebfe7c88df615ad7cbd0fb
Zachary Roux <zachary.roux@nextthought.com>
2017-01-20 21:07:55 +0000

    NTI-2868 and NTI-2867: Decimal grades are handled correctly, and result cannot be less than 0 or greater than 1.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@104923 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/grading/policies/simple.py
+++ src/nti/app/products/gradebook/grading/policies/simple.py
@@ -40,127 +40,133 @@ from nti.schema.fieldproperty import createDirectFieldProperties
 
 @interface.implementer(ISimpleTotalingGradingPolicy)
 class SimpleTotalingGradingPolicy(DefaultCourseGradingPolicy):
-    __metaclass__ = MetaGradeBookObject
-    createDirectFieldProperties(ISimpleTotalingGradingPolicy)
+	__metaclass__ = MetaGradeBookObject
+	createDirectFieldProperties(ISimpleTotalingGradingPolicy)
 
-    PresentationGradeScheme = None
-    presentation = alias('PresentationGradeScheme')
+	PresentationGradeScheme = None
+	presentation = alias('PresentationGradeScheme')
 
-    def __init__(self, *args, **kwargs):
-        DefaultCourseGradingPolicy.__init__(self, *args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		DefaultCourseGradingPolicy.__init__(self, *args, **kwargs)
 
-    @readproperty
-    def book(self):
-        return IGradeBook(self.course)
+	@readproperty
+	def book(self):
+		return IGradeBook(self.course)
 
-    @readproperty
-    def dateContext(self):
-        return IQAssignmentDateContext(self.course, None)
+	@readproperty
+	def dateContext(self):
+		return IQAssignmentDateContext(self.course, None)
 
-    def validate(self):
-        if self.grader is not None:
-            self.grader.validate()
+	def validate(self):
+		if self.grader is not None:
+			self.grader.validate()
 
-    def verify(self, book=None):
-        return True
+	def verify(self, book=None):
+		return True
 
-    def _is_due(self, assignment, now):
-        dates = self.dateContext
-        if assignment is not None and dates is not None:
-            _ending = dates.of(assignment).available_for_submission_ending
-            return bool(_ending and now > _ending)
-        return False
+	def _is_due(self, assignment, now):
+		dates = self.dateContext
+		if assignment is not None and dates is not None:
+			_ending = dates.of(assignment).available_for_submission_ending
+			return bool(_ending and now > _ending)
+		return False
 
-    def grade(self, principal, *args, **kwargs):
-        now = datetime.utcnow()
-        total_points_earned = 0
-        total_points_available = 0
+	def grade(self, principal, *args, **kwargs):
+		now = datetime.utcnow()
+		total_points_earned = 0
+		total_points_available = 0
 
-        gradebook_assignment_ids = set()
-        assignment_policies = get_assignment_policies(self.course)
-        username = IPrincipal(principal).id
+		gradebook_assignment_ids = set()
+		assignment_policies = get_assignment_policies(self.course)
+		username = IPrincipal(principal).id
 
-        for grade in self.book.iter_grades(username):
-            gradebook_assignment_ids.add(grade.AssignmentId)
-            excused = IExcusedGrade.providedBy(grade)
-            if not excused:
-                ntiid = grade.AssignmentId
-                total_points = self._get_total_points_for_assignment(ntiid,
-                                                                     assignment_policies)
-                if not total_points:
-                    # If an assignment doesn't have a total_point value, we
-                    # ignore it.
-                    continue
+		for grade in self.book.iter_grades(username):
+			gradebook_assignment_ids.add(grade.AssignmentId)
+			excused = IExcusedGrade.providedBy(grade)
+			if not excused:
+				ntiid = grade.AssignmentId
+				total_points = self._get_total_points_for_assignment(ntiid,
+																	 assignment_policies)
+				if not total_points:
+					# If an assignment doesn't have a total_point value, we
+					# ignore it.
+					continue
 
-                earned_points = self._get_earned_points_for_assignment(grade)
-                if earned_points is None:
-                    # If for some reason we couldn't convert this grade
-                    # to an int, we ignore this assignment.
-                    continue
+				earned_points = self._get_earned_points_for_assignment(grade)
+				if earned_points is None:
+					# If for some reason we couldn't convert this grade
+					# to an int, we ignore this assignment.
+					continue
 
-                total_points_available += total_points
-                total_points_earned += earned_points
+				total_points_available += total_points
+				total_points_earned += earned_points
 
-        # Now fetch assignments we haven't seen that are past due.
-        all_assignments = self._get_all_assignments_for_user(self.course,
-                                                             principal)
+		# Now fetch assignments we haven't seen that are past due.
+		all_assignments = self._get_all_assignments_for_user(self.course,
+															 principal)
 
-        for assignment in all_assignments:
-            ntiid = assignment.ntiid
-            if      self._is_due(assignment, now) \
-                and not ntiid in gradebook_assignment_ids:
-                total_points = self._get_total_points_for_assignment(ntiid,
-                                                                     assignment_policies)
-                if total_points:
-                    total_points_available += total_points
+		for assignment in all_assignments:
+			ntiid = assignment.ntiid
+			if	  self._is_due(assignment, now) \
+				and not ntiid in gradebook_assignment_ids:
+				total_points = self._get_total_points_for_assignment(ntiid,
+																	 assignment_policies)
+				if total_points:
+					total_points_available += total_points
 
-        if total_points_available == 0:
-            # There are no assignments due with assigned point values,
-            # so just return none because we can't meaningfully
-            # predict any grade for this case.
-            return None
+		if total_points_available == 0:
+			# There are no assignments due with assigned point values,
+			# so just return none because we can't meaningfully
+			# predict any grade for this case.
+			return None
 
-        result = float(total_points_earned) / total_points_available
-        return round(result, 2)
+		result = float(total_points_earned) / total_points_available
+		# Results should be bounded to be within [0, 1]. 
+		if result < 0:
+			result = 0
+		elif result > 1:
+			result = 1
+			
+		return round(result, 2)
 
-    def _get_all_assignments_for_user(self, course, user):
-        catalog = ICourseAssignmentCatalog(course)
-        uber_filter = get_course_assessment_predicate_for_user(user, course)
-        # Must grab all assignments in our parent
-        assignments = catalog.iter_assignments(course_lineage=True)
-        return tuple(x for x in assignments if uber_filter(x))
+	def _get_all_assignments_for_user(self, course, user):
+		catalog = ICourseAssignmentCatalog(course)
+		uber_filter = get_course_assessment_predicate_for_user(user, course)
+		# Must grab all assignments in our parent
+		assignments = catalog.iter_assignments(course_lineage=True)
+		return tuple(x for x in assignments if uber_filter(x))
 
-    def _get_total_points_for_assignment(self, assignment_id, assignment_policies):
-        result = None
-        try:
-            policy = assignment_policies[assignment_id]
-        except KeyError:
-            # If there is no entry for this assignment, we ignore it.
-            pass
-        else:
-            autograde_policy = policy.get('auto_grade', None)
-            if autograde_policy:
-                # If we have an autograde entry with total_points,
-                # return total_points. If it does not have total_points,
-                # or if no autograde entry exists, we return 0.
-                result = autograde_policy.get('total_points', None)
-        if not result:
-            logger.warn(
-                'Assignment without total_points cannot be part of grade policy (%s) (%s)',
-                assignment_id,
-                result)
-        return result
+	def _get_total_points_for_assignment(self, assignment_id, assignment_policies):
+		result = None
+		try:
+			policy = assignment_policies[assignment_id]
+		except KeyError:
+			# If there is no entry for this assignment, we ignore it.
+			pass
+		else:
+			autograde_policy = policy.get('auto_grade', None)
+			if autograde_policy:
+				# If we have an autograde entry with total_points,
+				# return total_points. If it does not have total_points,
+				# or if no autograde entry exists, we return 0.
+				result = autograde_policy.get('total_points', None)
+		if not result:
+			logger.warn(
+				'Assignment without total_points cannot be part of grade policy (%s) (%s)',
+				assignment_id,
+				result)
+		return result
 
-    def _get_earned_points_for_assignment(self, grade):
-        try:
-            value = grade.value
-            if isinstance(value, string_types):
-                value = value.strip()
-                if value.endswith('-'):
-                    value = value[:-1]
-            return int(value)
-        except (ValueError, TypeError):
-            logger.warn('Gradebook entry without valid point value (%s) (%s)',
-                        grade.value,
-                        grade.AssignmentId)
-            return None
+	def _get_earned_points_for_assignment(self, grade):
+		try:
+			value = grade.value
+			if isinstance(value, string_types):
+				value = value.strip()
+				if value.endswith('-'):
+					value = value[:-1]
+			return float(value)
+		except (ValueError, TypeError):
+			logger.warn('Gradebook entry without valid point value (%s) (%s)',
+						grade.value,
+						grade.AssignmentId)
+			return None
