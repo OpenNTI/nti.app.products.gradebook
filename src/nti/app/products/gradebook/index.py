ec0f56c381abbde6619875ac8acc203e1e3d5fca
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-01 21:27:50 +0000

    pep8; opt a bit; use proper unicode conversion
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@105665 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/index.py
+++ src/nti/app/products/gradebook/index.py
@@ -19,6 +19,8 @@ from zope.location import locate
 
 from nti.app.products.gradebook.interfaces import IGrade
 
+from nti.common.string import to_unicode
+
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 
@@ -48,143 +50,153 @@ IX_STUDENT = IX_USERNAME = 'username'
 IX_CREATOR = IX_INSTRUCTOR = 'creator'
 IX_COURSE = IX_ENTRY = IX_GRADE_COURSE = 'gradeCourse'
 
-class AssignmentIdIndex(ValueIndex):
-	default_field_name = 'AssignmentId'
-	default_interface = IGrade
 
-	def index_doc(self, docid, obj):
-		if not self.interface.providedBy(obj):
-			return None
-		return super(AssignmentIdIndex, self).index_doc(docid, obj)
+class AssignmentIdIndex(ValueIndex):
+    default_field_name = 'AssignmentId'
+    default_interface = IGrade
+
+    def index_doc(self, docid, obj):
+        if not self.interface.providedBy(obj):
+            return None
+        return super(AssignmentIdIndex, self).index_doc(docid, obj)
+
 
 class CreatorRawIndex(RawValueIndex):
-	pass
+    pass
+
 
 def CreatorIndex(family=None):
-	return NormalizationWrapper(field_name='creator_username',
-								interface=ICreatedUsername,
-								index=CreatorRawIndex(family=family),
-								normalizer=StringTokenNormalizer())
+    return NormalizationWrapper(field_name='creator_username',
+                                interface=ICreatedUsername,
+                                index=CreatorRawIndex(family=family),
+                                normalizer=StringTokenNormalizer())
+
 
 class UsernameRawIndex(RawValueIndex):
-	pass
+    pass
+
 
 def UsernameIndex(family=None):
-	return NormalizationWrapper(field_name='Username',
-								interface=IGrade,
-								index=UsernameRawIndex(family=family),
-								normalizer=StringTokenNormalizer())
+    return NormalizationWrapper(field_name='Username',
+                                interface=IGrade,
+                                index=UsernameRawIndex(family=family),
+                                normalizer=StringTokenNormalizer())
+
 
 class GradeValueIndex(ValueIndex):
-	default_field_name = 'value'
-	default_interface = IGrade
+    default_field_name = 'value'
+    default_interface = IGrade
 
-	def index_doc(self, docid, obj):
-		if not self.interface.providedBy(obj):
-			return None
-		return super(GradeValueIndex, self).index_doc(docid, obj)
+    def index_doc(self, docid, obj):
+        if not self.interface.providedBy(obj):
+            return None
+        return super(GradeValueIndex, self).index_doc(docid, obj)
+
 
 class ValidatingGradeValueType(object):
-	"""
-	The "interface" we adapt to to find the grade value type.
-	"""
+    """
+    The "interface" we adapt to to find the grade value type.
+    """
 
-	__slots__ = (b'type',)
+    __slots__ = (b'type',)
 
-	def __init__(self, obj, default=None):
-		grade = IGrade(obj, default)
-		value = getattr(grade, 'value', None)
-		if value is not None:
-			self.type = unicode(value.__class__.__name__)
+    def __init__(self, obj, default=None):
+        grade = IGrade(obj, default)
+        value = getattr(grade, 'value', None)
+        if value is not None:
+            self.type = to_unicode(value.__class__.__name__)
 
-	def __reduce__(self):
-		raise TypeError()
+    def __reduce__(self):
+        raise TypeError()
+
 
 class GradeValueTypeIndex(ValueIndex):
-	default_field_name = 'type'
-	default_interface = ValidatingGradeValueType
+    default_field_name = 'type'
+    default_interface = ValidatingGradeValueType
+
 
 class ValidatingGradeSite(object):
-	"""
-	The "interface" we adapt to to find the grade value course.
-	"""
+    """
+    The "interface" we adapt to to find the grade value course.
+    """
 
-	__slots__ = (b'site',)
+    __slots__ = (b'site',)
 
-	def __init__(self, obj, default=None):
-		grade = IGrade(obj, default)
-		folder = find_interface(grade, 
-								IHostPolicyFolder, 
-								strict=False) if grade is not None else None
-		if folder is not None:
-			self.site = unicode(folder.__name__)
+    def __init__(self, obj, default=None):
+        grade = IGrade(obj, default)
+        folder = find_interface(grade, IHostPolicyFolder, strict=False)
+        if folder is not None:
+            self.site = to_unicode(folder.__name__)
 
-	def __reduce__(self):
-		raise TypeError()
+    def __reduce__(self):
+        raise TypeError()
+
 
 class SiteIndex(ValueIndex):
-	default_field_name = 'site'
-	default_interface = ValidatingGradeSite
+    default_field_name = 'site'
+    default_interface = ValidatingGradeSite
+
 
 class ValidatingGradeCatalogEntryID(object):
-	"""
-	The "interface" we adapt to to find the grade value course.
-	"""
+    """
+    The "interface" we adapt to to find the grade value course.
+    """
 
-	__slots__ = (b'ntiid',)
+    __slots__ = (b'ntiid',)
 
-	def __init__(self, obj, default=None):
-		grade = IGrade(obj, default)
-		course = find_interface(grade, 
-								ICourseInstance, 
-								strict=False) if grade is not None else None
-		entry = ICourseCatalogEntry(course, None)  # entry is an annotation
-		if entry is not None:
-			self.ntiid = unicode(entry.ntiid)
+    def __init__(self, obj, default=None):
+        grade = IGrade(obj, default)
+        course = find_interface(grade, ICourseInstance, strict=False)
+        entry = ICourseCatalogEntry(course, None)  # entry is an annotation
+        if entry is not None:
+            self.ntiid = to_unicode(entry.ntiid)
 
-	def __reduce__(self):
-		raise TypeError()
+    def __reduce__(self):
+        raise TypeError()
+
 
 class CatalogEntryIDIndex(ValueIndex):
-	default_field_name = 'ntiid'
-	default_interface = ValidatingGradeCatalogEntryID
+    default_field_name = 'ntiid'
+    default_interface = ValidatingGradeCatalogEntryID
+
 
 @interface.implementer(IMetadataCatalog)
 class MetadataGradeCatalog(Catalog):
 
-	super_index_doc = Catalog.index_doc
+    super_index_doc = Catalog.index_doc
 
-	def index_doc(self, docid, ob):
-		pass
+    def index_doc(self, docid, ob):
+        pass
 
-	def force_index_doc(self, docid, ob):
-		self.super_index_doc(docid, ob)
+    def force_index_doc(self, docid, ob):
+        self.super_index_doc(docid, ob)
+
 
 def install_grade_catalog(site_manager_container, intids=None):
-	lsm = site_manager_container.getSiteManager()
-	intids = lsm.getUtility(IIntIds) if intids is None else intids
-	catalog = lsm.queryUtility(IMetadataCatalog, name=CATALOG_NAME)
-	if catalog is not None:
-		return catalog
+    lsm = site_manager_container.getSiteManager()
+    intids = lsm.getUtility(IIntIds) if intids is None else intids
+    catalog = lsm.queryUtility(IMetadataCatalog, name=CATALOG_NAME)
+    if catalog is not None:
+        return catalog
 
-	catalog = MetadataGradeCatalog(family=intids.family)
-	locate(catalog, site_manager_container, CATALOG_NAME)
-	intids.register(catalog)
-	lsm.registerUtility(catalog, provided=IMetadataCatalog, name=CATALOG_NAME)
+    catalog = MetadataGradeCatalog(family=intids.family)
+    locate(catalog, site_manager_container, CATALOG_NAME)
+    intids.register(catalog)
+    lsm.registerUtility(catalog, provided=IMetadataCatalog, name=CATALOG_NAME)
 
-	for name, clazz in ((IX_SITE, SiteIndex),
-						(IX_CREATOR, CreatorIndex),
-						(IX_USERNAME, UsernameIndex),
-						(IX_GRADE_VALUE, GradeValueIndex),
-						(IX_GRADE_TYPE, GradeValueTypeIndex),
-						(IX_ASSIGNMENT_ID, AssignmentIdIndex),
-						(IX_GRADE_COURSE, CatalogEntryIDIndex)):
-		index = clazz(family=intids.family)
-		intids.register(index)
-		locate(index, catalog, name)
-		catalog[name] = index
+    for name, clazz in ((IX_SITE, SiteIndex),
+                        (IX_CREATOR, CreatorIndex),
+                        (IX_USERNAME, UsernameIndex),
+                        (IX_GRADE_VALUE, GradeValueIndex),
+                        (IX_GRADE_TYPE, GradeValueTypeIndex),
+                        (IX_ASSIGNMENT_ID, AssignmentIdIndex),
+                        (IX_GRADE_COURSE, CatalogEntryIDIndex)):
+        index = clazz(family=intids.family)
+        intids.register(index)
+        locate(index, catalog, name)
+        catalog[name] = index
 
-	return catalog
+    return catalog
 
 # deprecated
 from zope.deprecation import deprecated
@@ -192,4 +204,4 @@ from zope.deprecation import deprecated
 deprecated("GradeCatalog", "use MetadataGradeCatalog")
 @interface.implementer(ICatalog)
 class GradeCatalog(Catalog):
-	pass
+    pass
