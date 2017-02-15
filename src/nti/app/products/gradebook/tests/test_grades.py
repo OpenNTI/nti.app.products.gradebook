26d825faf8d4f8281eb501a3a8688a12eafcf90f
Zachary Roux <zachary.roux@nextthought.com>
2017-02-15 22:36:46 +0000

    Update externalization test for predicted grades to include DisplayableGrade field
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@106675 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/tests/test_grades.py
+++ src/nti/app/products/gradebook/tests/test_grades.py
@@ -27,6 +27,7 @@ from nti.app.products.gradebook.grades import Grade
 from nti.app.products.gradebook.grades import GradeWeakRef
 from nti.app.products.gradebook.grades import PersistentGrade
 from nti.app.products.gradebook.grades import PredictedGrade
+from nti.app.products.gradebook.gradescheme import LetterNumericGradeScheme
 
 from nti.app.products.gradebook.interfaces import IGrade
 
@@ -38,103 +39,119 @@ from nti.externalization.externalization import to_external_object
 
 from nti.app.products.gradebook.tests import SharedConfiguringTestLayer
 
+
 class TestGrades(unittest.TestCase):
-	
-	layer = SharedConfiguringTestLayer
 
-	def test_implements(self):
-		now = time.time()
+    layer = SharedConfiguringTestLayer
 
-		grade = PersistentGrade()
-		grade.__name__ = 'foo@bar'
+    def test_implements(self):
+        now = time.time()
 
-		assert_that( grade, validly_provides(IGrade) )
+        grade = PersistentGrade()
+        grade.__name__ = 'foo@bar'
 
-		assert_that( grade, has_property( 'createdTime', grade.lastModified ))
-		assert_that( grade, has_property( 'lastModified', greater_than_or_equal_to(now)))
+        assert_that(grade, validly_provides(IGrade))
 
-		grade.createdTime = 1
-		assert_that( grade, has_property( 'createdTime', 1 ))
+        assert_that(grade, has_property('createdTime', grade.lastModified))
+        assert_that(
+            grade, has_property('lastModified', greater_than_or_equal_to(now)))
 
-	def test_unpickle_old_state(self):
+        grade.createdTime = 1
+        assert_that(grade, has_property('createdTime', 1))
 
-		for clazz in (Grade, PersistentGrade):
-			grade = clazz()
-			grade.__name__ = 'foo@bar'
-	
-			state = grade.__dict__.copy()
-			del state['createdTime']
-	
-			grade = Grade.__new__(Grade)
-			grade.__setstate__(state)
-	
-			assert_that( grade, has_property( 'createdTime', grade.lastModified ))
+    def test_unpickle_old_state(self):
 
-	def test_wref(self):
-		assert_that( calling(GradeWeakRef).with_args(Grade()),
-					 raises(TypeError))
+        for clazz in (Grade, PersistentGrade):
+            grade = clazz()
+            grade.__name__ = 'foo@bar'
 
-		grade = Grade()
-		grade.__name__ = 'foo@bar'
-		column = grade.__parent__ = _GradeBookEntry()
+            state = grade.__dict__.copy()
+            del state['createdTime']
 
-		wref = GradeWeakRef(grade)
-		assert_that( wref, validly_provides(IWeakRef) )
+            grade = Grade.__new__(Grade)
+            grade.__setstate__(state)
 
-		assert_that( wref, is_( GradeWeakRef(grade)))
+            assert_that(grade, has_property('createdTime', grade.lastModified))
 
-		d = {}
-		d[wref] = 1
-		assert_that( d, has_entry( GradeWeakRef(grade), 1 ))
+    def test_wref(self):
+        assert_that(calling(GradeWeakRef).with_args(Grade()),
+                    raises(TypeError))
 
-		# No part in gradebook yet, cannot resolve
-		assert_that( wref(), is_( none() ))
+        grade = Grade()
+        grade.__name__ = 'foo@bar'
+        column = grade.__parent__ = _GradeBookEntry()
 
-		column[grade.Username] = grade
-		assert_that( wref(), is_( same_instance( grade )))
+        wref = GradeWeakRef(grade)
+        assert_that(wref, validly_provides(IWeakRef))
 
-		assert_that( pickle.loads(pickle.dumps(wref)), is_(wref))
-		
-	def test_externalization_predicted_grade(self):
-		
-		predicted_grade = PredictedGrade(points_earned=1, points_available=2)
-		ext = to_external_object(predicted_grade)
-		assert_that(ext['Correctness'], is_(50))
-		assert_that(ext['PointsAvailable'], is_(2))
-		assert_that(ext['PointsEarned'], is_(1))
-		
-		predicted_grade = PredictedGrade(raw_value=0.75)
-		ext = to_external_object(predicted_grade)
-		assert_that(ext['Correctness'], is_(75))
-		assert_that(ext['PointsAvailable'], is_(None))
-		assert_that(ext['PointsEarned'], is_(None))
+        assert_that(wref, is_(GradeWeakRef(grade)))
 
-		predicted_grade = PredictedGrade(points_earned=1, points_available=0)
-		ext = to_external_object(predicted_grade)
-		# This situation doesn't make any sense, 
-		# so we just don't predict correctness.
-		assert_that(ext['Correctness'], is_(None))
-		assert_that(ext['PointsAvailable'], is_(0))
-		assert_that(ext['PointsEarned'], is_(1))
-		
+        d = {}
+        d[wref] = 1
+        assert_that(d, has_entry(GradeWeakRef(grade), 1))
+
+        # No part in gradebook yet, cannot resolve
+        assert_that(wref(), is_(none()))
+
+        column[grade.Username] = grade
+        assert_that(wref(), is_(same_instance(grade)))
+
+        assert_that(pickle.loads(pickle.dumps(wref)), is_(wref))
+
+    def test_externalization_predicted_grade(self):
+
+        predicted_grade = PredictedGrade(points_earned=1, points_available=2)
+        ext = to_external_object(predicted_grade)
+        assert_that(ext['Correctness'], is_(50))
+        assert_that(ext['DisplayableGrade'], is_(50))
+        assert_that(ext['PointsAvailable'], is_(2))
+        assert_that(ext['PointsEarned'], is_(1))
+
+        predicted_grade = PredictedGrade(raw_value=0.75)
+        ext = to_external_object(predicted_grade)
+        assert_that(ext['Correctness'], is_(75))
+        assert_that(ext['DisplayableGrade'], is_(75))
+        assert_that(ext['PointsAvailable'], is_(None))
+        assert_that(ext['PointsEarned'], is_(None))
+
+        predicted_grade = PredictedGrade(points_earned=1, points_available=0)
+        ext = to_external_object(predicted_grade)
+        # This situation doesn't make any sense,
+        # so we just don't predict correctness.
+        assert_that(ext['Correctness'], is_(None))
+        assert_that(ext['DisplayableGrade'], is_(None))
+        assert_that(ext['PointsAvailable'], is_(0))
+        assert_that(ext['PointsEarned'], is_(1))
+
+        # By default, DisplayableGrade should be the same
+        # as Correctness. However, if we're using a grading
+        # scheme, then it should display a formatted value
+        # according to that scheme.
+        predicted_grade = PredictedGrade(raw_value=0.75)
+        predicted_grade.Presentation = LetterNumericGradeScheme()
+        ext = to_external_object(predicted_grade)
+        assert_that(ext['DisplayableGrade'], is_('C 75'))
+
+
 class _GradeBookEntry(GradeBookEntry):
-	
-	def __conform__(self, iface):
-		return _CheapWref(self)
-	
+
+    def __conform__(self, iface):
+        return _CheapWref(self)
+
 from zope import interface
+
 
 @interface.implementer(IWeakRef)
 class _CheapWref(object):
-	
-	def __init__( self, gbe ):
-		self.gbe = gbe
 
-	def __call__(self):
-		return self.gbe
+    def __init__(self, gbe):
+        self.gbe = gbe
 
-	def __eq__(self, other):
-		return True
+    def __call__(self):
+        return self.gbe
 
-	def __hash__(self):
-		return 42
+    def __eq__(self, other):
+        return True
+
+    def __hash__(self):
+        return 42
