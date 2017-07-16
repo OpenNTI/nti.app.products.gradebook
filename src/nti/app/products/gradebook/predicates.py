d3a126d09dc11a1ec546f00729cd5dc29358b503
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-16 02:54:22 +0000

    use new methods in base class
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.gradebook/trunk@117541 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/gradebook/predicates.py
+++ src/nti/app/products/gradebook/predicates.py
@@ -11,27 +11,28 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 
+from nti.app.products.gradebook.interfaces import IGrade
 from nti.app.products.gradebook.interfaces import IGradeBook
 
 from nti.contenttypes.courses.interfaces import ICourseCatalog
 from nti.contenttypes.courses.interfaces import ICourseInstance
-from nti.contenttypes.courses.interfaces import IPrincipalEnrollments
 
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import ISystemUserPrincipal
 
 from nti.dataserver.metadata.predicates import BasePrincipalObjects
 
-from nti.site.hostpolicy import run_job_in_all_host_sites
 
-
-def gradebook_collector(user):
-    for enrollments in component.subscribers((user,), IPrincipalEnrollments):
-        for enrollment in enrollments.iter_enrollments():
-            course = ICourseInstance(enrollment, None)
-            book = IGradeBook(course, None)
-            if book is not None:
-                yield book
+def course_gradebook_objects(course):
+    book = IGradeBook(ICourseInstance(course, None), None)
+    if book is not None:
+        yield book
+        for part in book.values():
+            yield part
+            for entry in part.values():
+                yield entry
+                for grade in entry.valuues():
+                    yield grade
 
 
 @component.adapter(IUser)
@@ -39,14 +40,14 @@ class _GradePrincipalObjects(BasePrincipalObjects):
 
     def iter_objects(self):
         result = []
-
-        def _collector():
-            for book in gradebook_collector(self.user):
-                for grade in book.iter_grades(self.user.username):
-                    result.append(grade)
-        run_job_in_all_host_sites(_collector)
-        for obj in result:
-            yield obj
+        catalog = component.queryUtility(ICourseCatalog)
+        if catalog is None or catalog.isEmpty():
+            return result
+        for entry in catalog.iterCatalogEntries():
+            for obj in course_gradebook_objects(entry):
+                if IGrade.providedBy(obj) and self.creator(obj) == self.username:
+                    result.append(obj)
+        return result
 
 
 @component.adapter(ISystemUserPrincipal)
@@ -54,22 +55,14 @@ class _GradeBookPrincipalObjects(BasePrincipalObjects):
 
     def iter_objects(self):
         result = []
-
-        def _collector():
-            catalog = component.queryUtility(ICourseCatalog)
-            if catalog is None or catalog.isEmpty():
-                return
-            for entry in catalog.iterCatalogEntries():
-                course = ICourseInstance(entry, None)
-                book = ICourseInstance(course, None)
-                if book is None:
-                    continue
-
-                for part in book.values():
-                    result.append(part)
-                    for entry in part.values():
-                        result.append(entry)
-
-        run_job_in_all_host_sites(_collector)
-        for obj in result:
-            yield obj
+        catalog = component.queryUtility(ICourseCatalog)
+        if catalog is None or catalog.isEmpty():
+            return result
+        for entry in catalog.iterCatalogEntries():
+            for obj in course_gradebook_objects(entry):
+                if IGrade.providedBy(obj):
+                    if self.is_system_username(self.creator(obj)):
+                        result.append(obj)
+                else:
+                    result.append(obj)
+        return result
