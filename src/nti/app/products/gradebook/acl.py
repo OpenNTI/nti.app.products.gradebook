7e2fa38705d35c53e95dddac64b41a9a896667f8
Carlos Sanchez <papachoco@gmail.com>
2017-11-07 22:00:33 -0600

    pylint

--- src/nti/app/products/gradebook/acl.py
+++ src/nti/app/products/gradebook/acl.py
@@ -6,10 +6,9 @@ Relating to ACL implementations for objects defined in this package.
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import component
 from zope import interface
@@ -32,6 +31,8 @@ from nti.dataserver.authorization_acl import ace_denying_all
 
 from nti.dataserver.interfaces import IACLProvider
 from nti.dataserver.interfaces import ALL_PERMISSIONS
+
+logger = __import__('logging').getLogger(__name__)
 
 
 @component.adapter(IGradeBook)
