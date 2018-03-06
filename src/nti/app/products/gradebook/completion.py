b9698503b2f7f9622284b45f56de1adb36ca8fca
Josh Zuech <josh.zuech@nextthought.com>
2018-03-06 16:59:28 -0600

    Add IProgress adapter for assignments

---
+++ src/nti/app/products/gradebook/completion.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Adapters for application-level events.
+
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import component
+from zope import interface
+
+from nti.app.assessment.common.policy import get_auto_grade_policy
+
+from nti.app.assessment.interfaces import IUsersCourseAssignmentHistory
+
+from nti.app.products.gradebook.interfaces import IGrade
+
+from nti.assessment.interfaces import IQAssignment
+
+from nti.contenttypes.completion.progress import Progress
+
+from nti.contenttypes.completion.interfaces import IProgress
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.dataserver.interfaces import IUser
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@component.adapter(IUser, IQAssignment, ICourseInstance)
+@interface.implementer(IProgress)
+def _assignment_progress(user, assignment, course):
+    """
+    Calculate the :class:`IProgress` for this user, assignment, course.
+    """
+    histories = component.queryMultiAdapter((course, user),
+                                            IUsersCourseAssignmentHistory)
+    item = submitted_date = None
+    submitted = False
+    try:
+        item = histories[assignment.ntiid]
+        submitted_date = item.created
+        submitted = True
+    except KeyError:
+        pass
+
+    grade_val = None
+    if item is not None:
+        grade = IGrade(item, None)
+        grade_val = getattr(grade, 'value', None)
+
+    total_points = None
+    policy = get_auto_grade_policy(assignment, course)
+    if policy:
+        try:
+            total_points = policy.get('total_points') or None
+        except AttributeError:
+            pass
+    progress = Progress(NTIID=assignment.ntiid,
+                        AbsoluteProgress=grade_val,
+                        MaxPossibleProgress=total_points,
+                        LastModified=submitted_date,
+                        HasProgress=submitted)
+    return progress
+
